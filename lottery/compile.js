1ff4a9bdd3de380c0528f87fcf3391559f7a712d
Vincent Moore <cosmiqwarrior@gmail.com>
2018-03-11 10:06:12 -0400

    Update compile.js

--- lottery/compile.js
+++ lottery/compile.js
@@ -6,5 +6,5 @@ const solc = require('solc');
 const lotteryPath = path.resolve(__dirname, 'contracts', 'lottery.sol');
 const source = fs.readFileSync(lotteryPath, 'utf8');
 
-//compile statement.sol
+//export solc module
 module.exports = solc.compile(source, 1).contracts[':Lottery'];

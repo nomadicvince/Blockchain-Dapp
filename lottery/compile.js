a10b71d9550fbaecbab643fb758ef191e8f8a728
Vincent Moore <cosmiqwarrior@gmail.com>
2018-03-11 10:04:51 -0400

    Added lottery contracts
    Added a contract that enables players to enter a lottery with a certain amount of ether. The manager of the lottery picks a winner, who receives the total amount of ether entered by all players.

---
+++ lottery/compile.js
@@ -0,0 +1,10 @@
+//variables
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const lotteryPath = path.resolve(__dirname, 'contracts', 'lottery.sol');
+const source = fs.readFileSync(lotteryPath, 'utf8');
+
+//compile statement.sol
+module.exports = solc.compile(source, 1).contracts[':Lottery'];
